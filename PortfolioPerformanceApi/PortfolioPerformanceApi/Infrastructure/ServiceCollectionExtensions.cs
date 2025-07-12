a22e07b50cc00599a69778c7dc555940835d2f51
Thushara Bandara <thusharathemalbandara@outlook.com>
2025-07-12 18:05:07 +0800

    feat: mediatr and ef core added

--- PortfolioPerformanceApi/PortfolioPerformanceApi/Infrastructure/ServiceCollectionExtensions.cs
+++ PortfolioPerformanceApi/PortfolioPerformanceApi/Infrastructure/ServiceCollectionExtensions.cs
@@ -1,9 +1,16 @@
-﻿namespace PortfolioPerformance.Api.Infrastructure
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioPerformance.Data;
+using System.Reflection;
+
+namespace PortfolioPerformance.Api.Infrastructure
 {
     public static class ServiceCollectionExtensions
     {
         public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddDbContext<PortfolioPerformanceContext>(opt => opt.UseInMemoryDatabase("PortfolioDb"));
+
 
             return services;
         }
